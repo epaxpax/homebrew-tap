105bff40546d34c99491939d3967e9fca732dab0
epaxpax <23380293+epaxpax@users.noreply.github.com>
2026-07-09 08:30:34 +0200

    transmission-remote-gui-macos 0.1.4

--- Casks/transmission-remote-gui-macos.rb
+++ Casks/transmission-remote-gui-macos.rb
@@ -1,6 +1,6 @@
 cask "transmission-remote-gui-macos" do
-  version "0.1.3"
-  sha256 "adce1eeaee93a5adb74eb5d39353453365bda456522d8d5860bd7d9c3c465768"
+  version "0.1.4"
+  sha256 "12b3536e3c473f8c116308153fdb0f7d6f910af8a53f7115c0a090bce41fb3ed"
 
   url "https://github.com/epaxpax/transmission-remote-gui/releases/download/v#{version}/Transmission.Remote.GUI.app.zip",
       verified: "github.com/epaxpax/transmission-remote-gui/"
