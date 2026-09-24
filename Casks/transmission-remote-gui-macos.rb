928c28949b272bc63a15c5a66aca5ef85f99c0c3
epaxpax <23380293+epaxpax@users.noreply.github.com>
2026-09-24 17:49:01 +0200

    transmission-remote-gui-macos: az elavult url verified: paraméter kivéve

--- Casks/transmission-remote-gui-macos.rb
+++ Casks/transmission-remote-gui-macos.rb
@@ -2,8 +2,7 @@ cask "transmission-remote-gui-macos" do
   version "0.1.6"
   sha256 "6a409c14f8fad008582787f7a84c1a891ea64eb8830e722b2b51a4c157150a3b"
 
-  url "https://github.com/epaxpax/transmission-remote-gui/releases/download/v#{version}/Transmission.Remote.GUI.app.zip",
-      verified: "github.com/epaxpax/transmission-remote-gui/"
+  url "https://github.com/epaxpax/transmission-remote-gui/releases/download/v#{version}/Transmission.Remote.GUI.app.zip"
   name "Transmission Remote GUI"
   desc "Native SwiftUI macOS remote GUI for the Transmission BitTorrent daemon"
   homepage "https://github.com/epaxpax/transmission-remote-gui"
